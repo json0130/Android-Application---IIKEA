af8deb68bfafe66775dfc4d4fd10785c3d523cab
Jay <json941@aucklanduni.ac.nz>
2024-04-04 16:08:40 +1300

    Have generated the cardview of each item and applied to recycler view in the list_activity

---
+++ app/src/main/java/com/example/iikeaapp/data/DataProvider.java
@@ -0,0 +1,12 @@
+package com.example.iikeaapp.data;
+
+import java.util.HashMap;
+public class DataProvider {
+    public static HashMap<String, Furniture> getFurnitureItems() {
+        HashMap<String, Furniture> furnitureItems = new HashMap<>();
+        furnitureItems.put("sofa", new Furniture("Sofa", R.drawable.sofa_image, 599.99));
+        furnitureItems.put("dining_table", new Furniture("Dining Table", R.drawable.table_image, 399.99));
+        furnitureItems.put("armchair", new Furniture("Armchair", R.drawable.armchair_image, 249.99));
+        return furnitureItems;
+    }
+}
