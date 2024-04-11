faf6c61b6909a879bbe1c8b0a110223946f6ef8d
Jay <json941@aucklanduni.ac.nz>
2024-04-12 02:11:48 +1200

    Worked on the Shopping cart design and checkout page UI design

--- app/src/main/java/com/example/iikeaapp/activities/CheckoutActivity.java
+++ app/src/main/java/com/example/iikeaapp/activities/CheckoutActivity.java
@@ -1,7 +1,5 @@
 package com.example.iikeaapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,24 +7,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.example.iikeaapp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.button.MaterialButton;
 
-public class CartActivity extends AppCompatActivity {
+public class CheckoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_checkout);
+
+        MaterialButton checkOutButton = findViewById(R.id.checkout_button);
+        checkOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CheckoutActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // nav bar
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setSelectedItemId(R.id.bottom_cart);
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.bottom_cart) {
+                startActivity(new Intent(getApplicationContext(), CartActivity.class));
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+                finish();
                 return true;
             } else if (item.getItemId() == R.id.bottom_home) {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -40,15 +55,6 @@ public class CartActivity extends AppCompatActivity {
                 return true;
             }
             return false;
-        });
-
-        MaterialButton shoppingCartButton = findViewById(R.id.checkout_button);
-        shoppingCartButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CartActivity.this, CheckoutActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
